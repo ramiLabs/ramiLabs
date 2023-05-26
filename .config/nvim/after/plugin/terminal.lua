26ac06c0577052b5730d316d1e300e1485c596e9
Rami <rami.charbel@imi-hydronic.com>
2023-05-26 15:46:45 +0200

    little quality of life improvement for terminal.

--- .config/nvim/after/plugin/terminal.lua
+++ .config/nvim/after/plugin/terminal.lua
@@ -1,10 +1,24 @@
 local ok, terminal = pcall(require,"terminal")
 if not ok then return end
 terminal.setup({
-    layout = { open_cmd = "botright new" },
+    layout = { open_cmd = "horizontal new" },
     cmd = { vim.o.shell },
     autoclose = false,
 })
 local opts = { noremap = true, silent = true }
 local bind = vim.keymap.set
 bind("t", '<C-\\><C-\\>', '<C-\\><C-n>', opts);
+
+vim.api.nvim_create_autocmd({ "WinEnter", "BufWinEnter", "TermOpen" }, {
+    callback = function(args)
+        if vim.startswith(vim.api.nvim_buf_get_name(args.buf), "term://") then
+            vim.cmd("startinsert")
+        end
+    end,
+})
+
+vim.api.nvim_create_autocmd("TermOpen", {
+    command = [[setlocal nonumber norelativenumber winhl=Normal:NormalFloat]]
+})
+local term_map = require("terminal.mappings")
+vim.keymap.set("n", "<leader>to", term_map.toggle)
